cb31683236eceb15ba079a4713cca78b9c750628
m.ishida <m.ishida@pro-japan.co.jp>
2017-08-25 16:47:08 +0900

    初回コミット

---
+++ BlockGame/Assets/Script/BlockController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour {
+
+	public float timeOut = 1.0f;
+	private float timeElapsed;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("ConSTART");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		DownKeyCheck ();
+
+		timeElapsed += Time.deltaTime;
+
+		if(timeElapsed >= timeOut) {
+
+			GameObject movingBlock = GameObject.Find ("MovingBlock");
+			if (movingBlock) {
+				Vector3 pos = movingBlock.transform.position;
+				pos.y -= 0.4f;
+				movingBlock.transform.position = pos;
+			}
+
+			timeElapsed = 0.0f;
+		}
+	}
+
+	void DownKeyCheck(){
+//		if (Input.anyKeyDown) {
+//			foreach (KeyCode code in Enum.GetValues(typeof(KeyCode))) {
+//				if (Input.GetKeyDown (code)) {
+//					//処理を書く
+//					Debug.Log (code);
+//					break;
+//				}
+//			}
+//		}
+
+		GameObject movingBlock = GameObject.Find ("MovingBlock");
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Vector3 pos = movingBlock.transform.position;
+			pos.x -= 0.4f;
+			movingBlock.transform.position = pos;
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			Vector3 pos = movingBlock.transform.position;
+			pos.x += 0.4f;
+			movingBlock.transform.position = pos;
+		}else {
+			Debug.Log ("ELSE");
+		}
+	}
+}
---
+++ BlockGame/Assets/Script/BlockGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGenerator : MonoBehaviour {
+
+	public GameObject iBlockPrefab;
+	public GameObject jBlockPrefab;
+	public GameObject lBlockPrefab;
+	public GameObject oBlockPrefab;
+	public GameObject sBlockPrefab;
+	public GameObject tBlockPrefab;
+	public GameObject zBlockPrefab;
+
+	public static List<GameObject> blocks = new List<GameObject>();
+
+	public bool movingBlock = false;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("GeneSTART");
+
+		blocks.Add (iBlockPrefab);
+		blocks.Add (jBlockPrefab);
+		blocks.Add (lBlockPrefab);
+		blocks.Add (oBlockPrefab);
+		blocks.Add (sBlockPrefab);
+		blocks.Add (tBlockPrefab);
+		blocks.Add (zBlockPrefab);
+
+		BlockGenerator.generateBlock ();
+
+//		blocks = new GameObject[iBlockPrefab,jBlockPrefab,lBlockPrefab,oBlockPrefab,sBlockPrefab,tBlockPrefab,zBlockPrefab];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	static public void generateBlock(){
+
+		//ランダムに7種類のブロックを生成
+		int rand = Random.Range (1,7);
+		GameObject block = Instantiate (blocks [rand], new Vector3 (0, 4.57f, 0), Quaternion.Euler (0, 0, 0));
+		block.name = "MovingBlock";
+	}
+}
